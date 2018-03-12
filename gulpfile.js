d75e56fa150099d0ccc211c1a35f0f301fc7622e
protoEvangelion <ryantgarant@gmail.com>
2018-03-12 14:38:08 -0700

    Refactor config ports

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,71 @@
-let browserSync = require('browser-sync').create();
-var fs = require("fs");
-let gulp = require('gulp');
-let gulp_compass = require('gulp-compass');
-let proxy = require('http-proxy-middleware');
+const browserSync = require('browser-sync').create();
+const fs = require('fs');
+const gulp = require('gulp');
+const gulp_compass = require('gulp-compass');
+const proxy = require('http-proxy-middleware');
 
+const defaultConfig = JSON.parse(fs.readFileSync('./default.config.json'));
+const customConfig = JSON.parse(fs.readFileSync('./custom.config.json'));
 
-var configFile = "./config.json";
-config = JSON.parse(fs.readFileSync(configFile));
+const config = Object.assign({}, defaultConfig, customConfig);
 
 gulp.task('serve', ['sass-compass'], function() {
-	browserSync.init({
-		proxy: "",
-		files: ['assets/**'],
-		serveStatic: ['assets'],
-		middleware: [
-			function(req, res, next) {
-				let target = 'http://' + req.headers.host.replace('localhost:3000', 'localhost');
-				proxy({
-					target
-				})(req, res, next);
-			}
-		]
-	});
+    browserSync.init({
+        proxy: '',
+        files: ['assets/**'],
+        serveStatic: ['assets'],
+        middleware: [
+            function(req, res, next) {
+                const host = req.headers.host.replace(
+                    config.BROWSER_SYNC_PORT,
+                    config.LIFERAY_SERVER_PORT
+                );
 
-	gulp.watch(config.LIFERAY_PLUGINS_SDK + "themes/osb-www-events-theme/docroot/_diffs/css/**/*.*", ['sass-compass']);
-	gulp.watch(config.LIFERAY_PLUGINS_SDK + "portlets/osb-www-marketing-events-portlet/docroot/events/css/**/*.*", ['sass-compass']);
-	gulp.watch(config.LIFERAY_PLUGINS_SDK + "portlets/osb-www-marketing-events-portlet/docroot/users/css/**/*.*", ['sass-compass']);
-	gulp.watch(config.LIFERAY_PLUGINS_SDK + "portlets/osb-www-marketing-events-portlet/docroot/agenda/css/**/*.*", ['sass-compass']);
-	gulp.watch("stylesheets/scss/**/*.scss", ['sass-compass'])
+                proxy({ target: `http://${host}` })(req, res, next);
+            },
+        ],
+    });
+
+    gulp.watch(
+        config.LIFERAY_PLUGINS_SDK + 'themes/osb-www-events-theme/docroot/_diffs/css/**/*.*',
+        ['sass-compass']
+    );
+    gulp.watch(
+        config.LIFERAY_PLUGINS_SDK +
+            'portlets/osb-www-marketing-events-portlet/docroot/events/css/**/*.*',
+        ['sass-compass']
+    );
+    gulp.watch(
+        config.LIFERAY_PLUGINS_SDK +
+            'portlets/osb-www-marketing-events-portlet/docroot/users/css/**/*.*',
+        ['sass-compass']
+    );
+    gulp.watch(
+        config.LIFERAY_PLUGINS_SDK +
+            'portlets/osb-www-marketing-events-portlet/docroot/agenda/css/**/*.*',
+        ['sass-compass']
+    );
+    gulp.watch('stylesheets/scss/**/*.scss', ['sass-compass']);
 });
 
 gulp.task('sass-compass', function() {
-	return gulp.src("stylesheets/scss/**/*.scss")
-		.pipe(gulp_compass({
-			sass: 'stylesheets/scss',
-			import_path: [
-				config.LIFERAY_PORTAL_SRC + "portal-web/docroot/html/css/common",
-				config.LIFERAY_PLUGINS_SDK + "portlets/osb-www-marketing-events-portlet/docroot/events/css",
-				config.LIFERAY_PLUGINS_SDK + "themes/osb-www-events-theme/docroot/_diffs/css"
-			]
-		}))
-		.pipe(gulp.dest("assets"))
-		.pipe(browserSync.stream());
+    return gulp
+        .src('stylesheets/scss/**/*.scss')
+        .pipe(
+            gulp_compass({
+                sass: 'stylesheets/scss',
+                import_path: [
+                    config.LIFERAY_PORTAL_SRC + 'portal-web/docroot/html/css/common',
+                    config.LIFERAY_PLUGINS_SDK +
+                        'portlets/osb-www-marketing-events-portlet/docroot/events/css',
+                    config.LIFERAY_PLUGINS_SDK + 'themes/osb-www-events-theme/docroot/_diffs/css',
+                ],
+            })
+        )
+        .pipe(gulp.dest('assets'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('default', function() {
-	gulp.run("serve");
+    gulp.run('serve');
 });
